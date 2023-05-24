f7f3b8caaac8118c6cd8bc32c372e48f833700fd
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2023-05-24 21:17:49 +0700

    membuat classs ibu dengan access modifier public dan terdiri dari 6 member

--- mainIA/ibu.h
+++ mainIA/ibu.h
@@ -1,4 +1,4 @@
-#ifdef IBU_H
+#ifndef IBU_H
 #define IBU_H
 
 class ibu {
@@ -6,14 +6,14 @@ public:
 	string nama;
 	vector<anak*> daftar_anak;
 
-    ibu(string pNama) :nama(pNama) {
-        cout << "Ibu" \"" << nama << "\" ada \n";
+    ibu(string pNama) :nama(pNama) { 
+        cout << "Ibu \"" << nama << "\" ada\n";
     }
     ~ibu() {
         cout << "Ibu \"" << nama << "\" tidak ada\n";
     }
     void tambahAnak(anak*);
-    void cetakanak();
+    void cetakAnak();
 };
 void ibu::tambahAnak(anak* pAnak) {
     daftar_anak.push_back(pAnak);
