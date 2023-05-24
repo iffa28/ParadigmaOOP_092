709bf6d9d9775de40b02fd5c14fd428df53efa16
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2023-05-24 11:02:36 +0700

    Add project files.

---
+++ Paradigmaoop/Paradigmaoop.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class orang {
+public:
+    string nama;
+     
+    orang(string pNama) :
+};
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
